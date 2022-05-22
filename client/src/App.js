f5cb1a99adefdc315d032906ba0214fc47b942ce
MichaelPaloka <paloka.michael@gmail.com>
2022-05-22 12:06:44 -0400

    Login and Registration works!

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import LoginAndRegistration from './components/LoginAndRegistration';
 
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route element={<LoginAndRegistration/>} path="/FinancialTimes" default/>
+          <Route element={<LoginAndRegistration/>} path="/financialtimes" default/>
         </Routes>
       </BrowserRouter>
     </div>
--- client/src/components/LoginAndRegistration.js
+++ client/src/components/LoginAndRegistration.js
@@ -28,7 +28,7 @@ const LoginAndRegistration = () => {
             .then( res => {
                 console.log(res);
                 console.log(res.data);
-                navigate("")
+                // navigate("/financialtimes/home")
             })
             .catch( err => {
                 console.log(err.response.data);
@@ -46,13 +46,105 @@ const LoginAndRegistration = () => {
         })
             .then( res => {
                 console.log(res);
-                navigate("/Gamepartyfinder/home")
+                // navigate("/financialtimes/home")
             })
             .catch( err => {
                 console.log(err.response.data);
             })
     }
 
+    return (
+        <div>
+            <div style={{ display: 'flex',justifyContent: 'space-between', alignItems: 'center', padding: 10 }}>
+                <h1 style={{color: "#72A0C1"}}>Financil Times</h1>
+                <form onSubmit={onLoginHandler} style={{ display: 'flex', alignItems: 'center' }}>
+                    <div class="row mb-3">
+                        {/* Email */}
+                        <div class="col">
+                            <label for="email" class="col-form-label">Email:</label>
+                                <div class="col-sm-10">
+                                    <input type="email" onChange = {(e) => setEmail(e.target.value)} class="form-control"></input>
+                                </div>
+                        </div>
+                        {/* Password */}
+                        <div class="col">
+                            <label for="password" class="col-form-label">Password:</label>
+                                <div class="col-sm-10">
+                                    <input type="password" onChange = {(e) => setPassword(e.target.value)} class="form-control"></input>
+                                </div>
+                        </div>
+                    </div>
+                    <input type={"submit"} value="Login" class="btn btn-outline-primary" style={{height: 50}}></input>
+                </form>
+            </div>
+            
+            
+            <div style={{display: 'flex',justifyContent: 'Center'}}>
+                <div>
+                    <form onSubmit={onRegisterHandler} style={{width: 1000, height: 500, marginTop:100, borderStyle: 'solid', borderRadius: 20,padding: 25 }}>
+                        <h3 style={{textAlign: 'center'}}>Create an Account Below!</h3>
+                        <div class = "row mb-3">
+                            {/* First Name */}
+                            <div class="col">
+                                <label for="firstName" class="col-form-label">First Name:</label>
+                                    <div class="col-sm-10">
+                                        <input type="text" onChange = {(e) => setFirstName(e.target.value)} class="form-control"></input>
+                                    </div>
+                            </div>
+                            {errors.firstName && (
+                                    <p style={{color: 'red'}}>{errors.firstName.message}</p>
+                            )}
+                            {/* Last Name */}
+                            <div class="col">
+                                <label for="lastName" class="col-form-label">Last Name:</label>
+                                    <div class="col-sm-10">
+                                        <input type="text" onChange = {(e) => setLastName(e.target.value)} class="form-control"></input>
+                                    </div>
+                            </div>
+                            {errors.lastName && (
+                                    <p style={{color: 'red'}}>{errors.lastName.message}</p>
+                            )}
+                        </div>
+                        <div class="row mb-3">
+                            {/* Email */}
+                            <div class="col">
+                                <label for="email" class="col-form-label">Email:</label>
+                                    <div class="col-sm-10">
+                                        <input type="email" onChange = {(e) => setEmail(e.target.value)} class="form-control"></input>
+                                    </div>
+                            </div>
+                            {errors.email && (
+                                    <p style={{color: 'red'}}>{errors.email.message}</p>
+                            )}
+                        </div>
+                        <div class="row mb-3">
+                            {/* Password */}
+                            <div class="col">
+                                <label for="password" class="col-form-label">Password:</label>
+                                    <div class="col-sm-10">
+                                        <input type="password" onChange = {(e) => setPassword(e.target.value)} class="form-control"></input>
+                                    </div>
+                            </div>
+                            {errors.password && (
+                                    <p style={{color: 'red'}}>{errors.password.message}</p>
+                            )}
+                            {/* Confirm Password */}
+                            <div class="col">
+                                <label for="confirmPassword" class="col-form-label">Confirm Password:</label>
+                                    <div class="col-sm-10">
+                                        <input type="password" onChange = {(e) => setConfirmPassword(e.target.value)} class="form-control"></input>
+                                    </div>
+                            </div>
+                            {errors.confirmPassword && (
+                                    <p style={{color: 'red'}}>{errors.confirmPassword.message}</p>
+                            )}
+                        </div>
+                        <input type={"submit"} value="Create Account" class="btn btn-outline-primary"></input>
+                    </form>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default LoginAndRegistration;
